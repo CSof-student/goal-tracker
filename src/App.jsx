3f347263fd1968743078df5e5c538adfb4445cb2
CSof-student <122061715+CSof-student@users.noreply.github.com>
2025-06-14 11:38:08 -0700

    can now make goals and view them, edit doesn't work but imma work on it later

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import { supabase } from './supabaseClient'
 
 function App() {
 
+
+  // useeffect allows you to run side effects ex: fetching data, in components
+  //the function inside useeffect runs when the component renders
+  // the second argument tells it to rerun
+  //basically when the page loads It grabs the data in the database
   useEffect(() => {
     fetchGoals()
   }, [])
@@ -19,7 +24,8 @@ function App() {
   const [unit,setUnit] = useState('')
   const [message,setMessage] = useState('')
 
-  const [goals,setGoals] = useState('')
+  const [goals,setGoals] = useState([])
+  const [editingGoalId, setEditingGoalID] = useState(null)
 
     async function fetchGoals(){
       const {data,error} = await supabase.from('goals').select('*')
@@ -28,33 +34,79 @@ function App() {
         setMessage('Error creating goal')
       } else {
         console.log('Fetched goals:',data)
-        setGoals(data)
+        if (data) {
+          setGoals(data)
+        } else{
+          setGoals([])
+        }
       }
     }
-  
+  //inserts data to the tables when it gets submitted
     async function handleSubmit(e) {
       e.preventDefault()
-      const {data,error} = await supabase.from('goals').insert([
-        {
-          title: title,
-          target_value: parseFloat(targetValue),
-          unit: unit
+      // if editting goal:
+      if (editingGoalId) {
+        const {data,error} = await supabase
+        .from('goals')
+        .update(
+          {
+            title: title,
+            target_value: parseFloat(targetValue),
+            unit: unit
+          })
+          .eq('id', editingGoalId)
+
+          if(error){
+            console.error(error)
+            setMessage('Error editing goal')
+          } else {
+            console.log(data)
+            setMessage('Goal edited successfully!')
+            setTitle('')
+            setTargetValue('')
+            setUnit('')
+            fetchGoals();
+          }
+      } else{
+        const {data,error} = await supabase.from('goals').insert([
+          {
+            title: title,
+            target_value: parseFloat(targetValue),
+            unit: unit
+          }
+        ], {returning: 'representation'})
+        
+        if(error){
+          console.error(error)
+          setMessage('Error creating goal')
+        } else {
+          console.log(data)
+          setMessage('Goal created successfully!')
+          setTitle('')
+          setTargetValue('')
+          setUnit('')
+          fetchGoals();
         }
-      ], {returning: 'representation'})
-      
-      if(error){
-        console.error(error)
-        setMessage('Error creating goal')
-      } else {
-        console.log(data)
-        setMessage('Goal created successfully!')
-        setTitle('')
-        setTargetValue('')
-        setUnit('')
       }
+      }
+      
+
+    //deletes data from the table
+    async function handleDelete(goalID){
+      await supabase.from('goals').delete().eq('id',goalID)
+      fetchGoals()
+    }
+
+    async function handleEdit(goal) {
+      setTitle(goal.title ?? '')
+      setTargetValue(goal.target_value ?? '')
+      setUnit(goal.unit ?? '')
+      setEditingGoalID(goal.id ?? '')
     }
 
 
+
+    //what the page displays
     return (
       <div style = {{padding: 20}}>
 
@@ -72,12 +124,25 @@ function App() {
             <label>Target Value</label>
             <input value={targetValue} onChange={e=>setTargetValue(e.target.value)}/>
           </div>
-          <button type='submit'>Create Goal</button>
+          <button type='submit'>
+            {editingGoalId ? 'Save Changes' : 'Create Goal'}
+          </button>
         </form>
 
       <p>{message}</p>
+      <h2>Saved Goals:</h2>
+
+      {goals.map(goal => (
+      <div key={goal.id}>
+        <h3>{goal.title}</h3>
+          <p>{goal.target_value} {goal.unit}</p>
+          <button onClick={() => handleDelete(goal.id)}>Delete</button>
+          <button onClick={() => handleEdit(goal.id)}>edit</button>
       </div>
-    )
+))}
+
+      </div>
+    ) 
 }
 
 
